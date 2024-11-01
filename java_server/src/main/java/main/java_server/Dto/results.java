1f1f55fca1fa9d7de17802bccffe95e002916a1e
Habeomsu <hbs5186@naver.com>
2024-11-01 11:30:36 +0900

    dto생성, 컨트롤러 분리

---
+++ java_server/src/main/java/main/java_server/Dto/results.java
@@ -0,0 +1,16 @@
+package main.java_server.Dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class results {
+    @JsonProperty("original_file_id")
+    private String originalFileId;
+    @JsonProperty("similarity_score")
+    private double similarityScore;
+    @JsonProperty("similar_tracks")
+    private List<semiTrack> similarTracks;
+}
