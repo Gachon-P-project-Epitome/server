3dc690079684ff577d99e27613219d1cffde67b7
Habeomsu <hbs5186@naver.com>
2024-11-01 11:13:12 +0900

    플라스크 더미데이터 추가

--- flask_server/models/similarity_model.py
+++ flask_server/models/similarity_model.py
@@ -3,10 +3,11 @@ def compute_similarity(file_path, genre):
     # 예시 데이터
     similarity_score = 0.85  # 임의의 유사도 점수
     similar_tracks = [
-        {'id': 'track_1', 'similarity': 0.9},
-        {'id': 'track_2', 'similarity': 0.8},
-        {'id': 'track_3', 'similarity': 0.75},
-        {'id': 'track_4', 'similarity': 0.7},
-        {'id': 'track_5', 'similarity': 0.65},
+        {'id': '37S86pw74OH8j96ZmMnrpR', 'similarity': 0.9},
+        {'id': '6oNLSQX8bcAdbCElZYju3v', 'similarity': 0.8},
+        {'id': '4fouWK6XVHhzl78KzQ1UjL', 'similarity': 0.75},
+        {'id': '1lTBkwEm0wim9RsMXqtqWy', 'similarity': 0.7},
+        {'id': '1SS0WlKhJewviwEDZ6dWj0', 'similarity': 0.65},
+        {'id': '5vNRhkKd0yEAg8suGBpjeY', 'similarity': 0.6},
     ]
     return similarity_score, similar_tracks
